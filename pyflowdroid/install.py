047a1b2268193e20b30ba66ca60f1882cbe77b6b
Gustavo Viera López <gvieralopez@gmail.com>
2022-04-25 19:26:28 +0200

    Refactor install script

--- pyflowdroid/install.py
+++ pyflowdroid/install.py
@@ -1,31 +1,39 @@
-import zipfile
 import os
+import logging
+import zipfile
 import urllib.request
-from settings import FLOWDROID_EXEC, ANDROID_FOLDER, APK_FOLDER
-
-fd_link = """https://github.com/secure-software-engineering/FlowDroid/releases/download/v2.7.1/soot-infoflow-cmd-jar-with-dependencies.jar"""
-sable_link = """https://github.com/Sable/android-platforms/archive/master.zip"""
-
+from pyflowdroid.consts import (
+    FLOWDROID_DOWNLOAD_URL,
+    SABLE_DOWNLOAD_URL,
+    DEFAULT_APK_FOLDER_NAME,
+    FLOWDROID_EXEC_NAME,
+    PYFLOWDROID_PATH,
+)
+from pathlib import Path
 
 def download_flowdroid():
-    urllib.request.urlretrieve (fd_link, FLOWDROID_EXEC )
+    flowdroid_path = Path(PYFLOWDROID_PATH, FLOWDROID_EXEC_NAME)
+    urllib.request.urlretrieve(FLOWDROID_DOWNLOAD_URL, flowdroid_path)
+
 
 def download_android():
-    urllib.request.urlretrieve (sable_link, 'master.zip')
-    with zipfile.ZipFile("master.zip","r") as zip_ref:
-        zip_ref.extractall(".")
-    os.remove('master.zip') 
-    if not ANDROID_FOLDER in os.listdir('.'):
-        os.rename('android-platforms-master', ANDROID_FOLDER)
-def create_apk_folder():
-    if not APK_FOLDER  in os.listdir('.'):
-        os.mkdir(APK_FOLDER)
+    github_zip = "master.zip"
+    urllib.request.urlretrieve(SABLE_DOWNLOAD_URL, github_zip)
+    with zipfile.ZipFile(github_zip, "r") as zip_ref:
+        zip_ref.extractall(PYFLOWDROID_PATH)
+    os.remove(github_zip)
 
-if __name__ == '__main__':
-    print('Downloading Flowdroid')
+
+def create_apk_folder():
+    DEFAULT_APK_PATH = Path(PYFLOWDROID_PATH, DEFAULT_APK_FOLDER_NAME)
+    DEFAULT_APK_PATH.mkdir(parents=True, exist_ok=True)
+
+
+def install_all():
+    logging.info("Downloading Flowdroid")
     download_flowdroid()
-    print('Downloading Android')
+    logging.info("Downloading Android")
     download_android()
-    print('Creating Directories')
+    logging.info("Creating Directories")
     create_apk_folder()
-    print('Instalation Complete')
+    logging.info("Instalation Complete")
--- pyflowdroid/settings.py
+++
@@ -1,5 +0,0 @@
-FLOWDROID_EXEC = "soot-infoflow-cmd-jar-with-dependencies.jar"
-ANDROID_FOLDER = "android"
-SOURCES_SINKS_FILE = "sources_sinks/large.txt"
-APK_FOLDER = "apk"
-LOG_FOLDER = "logs"
